66a92edabf56f900f77651fa1cfaeb34e8fd35eb
Thomas Weyn <thomas@weynwebworks.com>
2015-10-19 14:40:20 +0200

    Fixed some stuff to work well with the new Signdirect layout. Might break things in other layouts

--- app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Block/Product/Price.php
+++ app/code/community/OrganicInternet/SimpleConfigurableProducts/Catalog/Block/Product/Price.php
@@ -2,14 +2,17 @@
 class OrganicInternet_SimpleConfigurableProducts_Catalog_Block_Product_Price
     extends Mage_Catalog_Block_Product_Price
 {
+    public $diplaypricefromlabel = false;
+    	
     #This is overridden as an admittedly nasty hack to not have to change the contents of catalog/product/price.phtml
     #This is because there's no nice way to keep price.phtml in sync between this extension and the magento core version
     #Yes, it's dependent on the value of $htmlToInsertAfter; I'm not aware of a better alternative.
     public function _toHtml() {
-        $htmlToInsertAfter = '<div class="price-box">';
-        if ($this->getTemplate() == 'catalog/product/price.phtml') {
+        if ($this->diplaypricefromlabel && $this->getTemplate() == 'catalog/product/price.phtml') {
             $product = $this->getProduct();
             if (is_object($product) && $product->isConfigurable()) {
+        		$htmlToInsertAfter = '<div class="price-box">';
+        
                 $extraHtml = '<span class="label" id="configurable-price-from-'
                 . $product->getId()
                 . $this->getIdSuffix()
